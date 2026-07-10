2e2a9d230e80cd3641115869a6ccbd1d40791d9f
ABHINAV-23-HUB <abhinavchaurasia456@gmail.com>
2026-07-10 15:14:16 +0530

    UPDATE .env , app.js , server.js file

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Smart Campus Event Management API"
+    });
+
+});
+
+module.exports = app;
